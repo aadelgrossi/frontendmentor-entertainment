7eb191a5e6b2e57306f9e71747ac02007a1bec16
Andre Grossi <iamandregrossi@gmail.com>
2023-02-18 12:09:17 -0300

    feat(components): Create Input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import type { Component } from 'solid-js';
 import Heading from './components/Heading';
+import Input from './components/Input';
 
 import './index.css';
 
+import search from './assets/icon-search.svg';
+
 const App: Component = () => {
   return (
-    <div class="h-screen bg-background">
+    <div class="h-screen bg-background px-10 flex flex-col gap-8">
       <Heading size="lg">My entertainment web app</Heading>
+      <Input placeholder="Search for Movies or TV Shows" />
+      <Input
+        size="lg"
+        icon={<img src={search} width="24px" height="24px" />}
+        placeholder="Search for Movies or TV Shows"
+      />
+      <Input
+        placeholder="Search for Movies or TV Shows"
+        error="Can't be empty"
+      />
+      <Input variant="border" placeholder="Email address" />
     </div>
   );
 };
---
+++ src/components/Input.tsx
@@ -0,0 +1,38 @@
+import { JSX, Show } from 'solid-js';
+
+interface InputProps extends JSX.InputHTMLAttributes<HTMLInputElement> {
+  icon?: JSX.Element;
+  size?: 'md' | 'lg';
+  error?: string;
+  variant?: 'ghost' | 'border';
+}
+
+const Input = (props: InputProps) => {
+  return (
+    <div class="flex gap-6 h-[37px] items-start relative ">
+      <Show when={props.icon}>
+        <span class="flex items-center h-full">{props.icon}</span>
+      </Show>
+      <input
+        class="bg-background w-full
+        border-transparent border-2 pb-[18px] focus:outline-none focus:border-b-secondary
+        font-light text-text caret-primary"
+        classList={{
+          ['border-b-secondary focus:border-b-text px-4']:
+            props.variant === 'border',
+          ['border-b-primary focus:border-b-primary']: Boolean(props?.error),
+          ['text-2xl']: props.size === 'lg',
+          ['text-[15px]']: props.size === 'md',
+        }}
+        {...props}
+      />
+      <Show when={props.error}>
+        <span class="absolute text-primary right-0 top-1 font-light text-[13px]">
+          {props.error}
+        </span>
+      </Show>
+    </div>
+  );
+};
+
+export default Input;
