b6199222d4964336d266593f695881bb001830e2
Andre Grossi <iamandregrossi@gmail.com>
2023-02-18 10:56:11 -0300

    feat(theme): Configure theme colors and font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,21 @@
+/* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#FC4747',
+        bg: '#10141E',
+        secondary: '#5A698F',
+        surface: '#161D2F',
+        text: '#FFFFFF',
+      },
+      fontFamily: {
+        sans: ['Outfit', 'sans-serif', 'system-ui'],
+      },
+    },
   },
   plugins: [],
-}
+};
