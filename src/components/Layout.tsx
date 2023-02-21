325ad5a57a4a89d5958000cf37e0aefd96763885
Andre Grossi <iamandregrossi@gmail.com>
2023-02-20 23:49:38 -0300

    style(Layout): Lint file and add overflow hidden rule

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,15 +1,15 @@
-import { ParentProps } from "solid-js";
-import Navbar from "./Navbar";
+import { ParentProps } from 'solid-js';
+import Navbar from './Navbar';
 
-const Layout = (props:ParentProps) => {
+const Layout = (props: ParentProps) => {
   return (
-    <div class="min-h-screen w-full bg-background flex flex-col text-text lg:flex-row">
+    <div class="min-h-screen overflow-x-hidden bg-background flex flex-col text-text lg:flex-row">
       <Navbar />
       <main class="w-full flex flex-col px-4 md:px-6 lg:px-10 mt-4 lg:mt-8 gap-10">
         {props.children}
       </main>
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
