51fcddac60112d7b6779a03e0231d9824f84b11c
Andre Grossi <iamandregrossi@gmail.com>
2023-02-21 19:13:32 -0300

    feat(home/Trending): Add debounce to index, fix carousel overflow
    issues and add gradient indicator at the right

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -6,9 +6,8 @@ interface SectionProps extends ParentProps {
 }
 const Section = (props: SectionProps) => {
   return (
-    <div class="flex flex-col w-full gap-6 lg:gap-8">
+    <div class="flex flex-col w-full gap-6 lg:gap-8 relative">
       <Heading size="lg">{props?.title || 'Recommended for you'}</Heading>
-
       {props.children}
     </div>
   );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { Component, createResource } from 'solid-js';
+import { createResource } from 'solid-js';
 import Input from '~/components/Input';
 
 import { createSignal, For } from 'solid-js';
@@ -6,8 +6,9 @@ import { createSignal, For } from 'solid-js';
 import search from '~/assets/icon-search.svg';
 import Card from '~/components/Card';
 import getData from '~/services/getData';
-import Heading from '~/components/Heading';
 import Section from '~/components/Section';
+
+import { debounce } from '@solid-primitives/scheduled';
 
 const Home = () => {
   const [query, setQuery] = createSignal('');
@@ -15,18 +16,22 @@ const Home = () => {
   const [trending] = createResource(() => getData(query(), true));
   const [recommended] = createResource(query, () => getData(query(), false));
 
+  const setDebouncedQuery = debounce((value: string) => setQuery(value), 500);
+
   return (
     <>
-      <Input
-        size="lg"
-        value={query()}
-        onInput={(e) => setQuery(e.currentTarget.value)}
-        icon={<img src={search} width="24px" height="24px" />}
-        placeholder="Search for Movies or TV Shows"
-      />
+      <span class="lg:mr-20">
+        <Input
+          size="lg"
+          value={query()}
+          onInput={(e) => setDebouncedQuery(e.currentTarget.value)}
+          icon={<img src={search} width="24px" height="24px" />}
+          placeholder="Search for Movies or TV Shows"
+        />
+      </span>
 
       <Section title="Trending">
-        <div class="carousel gap-4 md:gap-10 lg:mr-40">
+        <div class="carousel lg:max-w-[88vw] gap-4 md:gap-10">
           <For each={trending()}>
             {(item) => (
               <Card
@@ -41,11 +46,12 @@ const Home = () => {
             )}
           </For>
         </div>
+        <div class="absolute right-0 bottom-0 h-[80%] w-20 bg-gradient-to-l from-background/90 to-transparent" />
       </Section>
 
       <Section>
         <div
-          class="grid lg:w-[90%] grid-cols-[repeat(auto-fill,minmax(180px,auto))] gap-4
+          class="grid grid-cols-[repeat(auto-fill,minmax(180px,auto))] gap-4
         md:grid-cols-[repeat(auto-fit,minmax(220px,auto))] md:gap-5
         lg:grid-cols-[repeat(auto-fill,minmax(260px,auto))] lg:gap-10"
         >
