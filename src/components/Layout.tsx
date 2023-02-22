f91830fcd11aeb98aabc0a97c3147587fbb5f0de
Andre Grossi <iamandregrossi@gmail.com>
2023-02-22 12:02:22 -0300

    feat(favicon): Add favicon package with app logo

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import Navbar from './Navbar';
 
 const Layout = (props: ParentProps) => {
   return (
-    <div class="min-h-screen bg-background flex flex-col text-text lg:flex-row">
+    <div class="min-h-screen overflow-x-hidden bg-background flex flex-col text-text lg:flex-row">
       <Navbar />
       <main class="w-full flex flex-col px-4 md:px-6 lg:px-10 mt-4 lg:mt-8 gap-10">
         {props.children}
