13fd9d6367360193b2e972d14e87869453a90639
Andre Grossi <iamandregrossi@gmail.com>
2023-02-22 11:43:28 -0300

    feat(store/bookmarks): Enable zustand persist middleware

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,6 @@
 import create from 'solid-zustand';
 
+import { persist } from 'zustand/middleware';
 interface BookmarksState {
   bookmarks: string[];
 }
@@ -8,21 +9,26 @@ interface BookmarksStore extends BookmarksState {
   toggleBookmark: (payload: string) => void;
 }
 
-const createBookmarksStore = create<BookmarksStore>((set, get) => ({
-  bookmarks: [],
-  toggleBookmark: (payload) => {
-    const isBookmarked = get().bookmarks.includes(payload);
+const persistedStore = persist<BookmarksStore>(
+  (set, get) => ({
+    bookmarks: [],
+    toggleBookmark: (payload) => {
+      const isBookmarked = get().bookmarks.includes(payload);
 
-    if (isBookmarked) {
-      return set({
-        bookmarks: get().bookmarks.filter(
-          (bookmarked) => bookmarked !== payload
-        ),
-      });
-    }
+      if (isBookmarked) {
+        return set({
+          bookmarks: get().bookmarks.filter(
+            (bookmarked) => bookmarked !== payload
+          ),
+        });
+      }
 
-    return set({ bookmarks: [...get().bookmarks, payload] });
-  },
-}));
+      return set({ bookmarks: [...get().bookmarks, payload] });
+    },
+  }),
+  { name: '@@entertainment:bookmarks' }
+);
+
+const createBookmarksStore = create(persistedStore);
 
 export default createBookmarksStore;
