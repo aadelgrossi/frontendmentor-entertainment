3f38e3fc7d15bfdb7216d2e01901d5b565ce3c69
Andre Grossi <iamandregrossi@gmail.com>
2023-02-24 14:06:30 -0300

    feat(Button): Use tw-merge to accept more classes as props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,19 @@
 import { ParentProps } from 'solid-js';
+import { twMerge } from 'tailwind-merge';
 
 interface ButtonProps extends ParentProps {
+  class?: string;
   onClick?: () => void;
 }
 
 const Button = (props: ButtonProps) => {
+  const classes =
+    twMerge(`flex items-center w-full h-12 justify-center p-4 bg-primary rounded-[6px]
+  transition-colors font-light
+  hover:bg-white text-white hover:text-surface-main ${props.class}`);
+
   return (
-    <button
-      children={props.children}
-      onClick={props.onClick}
-      class="flex items-center w-full h-12 justify-center p-4 bg-primary rounded-[6px]
-      transition-colors font-light
-      hover:bg-white text-white hover:text-surface-main"
-    />
+    <button children={props.children} onClick={props.onClick} class={classes} />
   );
 };
 
