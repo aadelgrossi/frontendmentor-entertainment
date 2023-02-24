980bcc1a9fed14341aa964f0ec9064281b251f1e
Andre Grossi <iamandregrossi@gmail.com>
2023-02-24 19:08:03 -0300

    fix(Input): Update classes when props.error is passed

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -8,6 +8,8 @@ interface InputProps extends JSX.InputHTMLAttributes<HTMLInputElement> {
 }
 
 const Input = (props: InputProps) => {
+  const hasError = Boolean(props?.error);
+
   return (
     <div class="flex gap-6 items-start relative">
       <Show when={props.icon}>
@@ -18,10 +20,12 @@ const Input = (props: InputProps) => {
         border-transparent border-2 pb-[18px] focus:outline-none focus:border-b-secondary
         font-light text-text caret-primary"
         classList={{
-          ['bg-transparent']: props.variant === 'border',
-          ['border-b-secondary focus:border-b-text px-4']:
+          ['focus:border-b-secondary']: !hasError,
+          ['bg-transparent px-4']: props.variant === 'border',
+          ['border-b-secondary focus:border-b-white']:
             props.variant === 'border',
-          ['border-b-primary focus:border-b-primary']: Boolean(props?.error),
+          ['border-b-primary focus:border-b-primary']:
+            props.variant === 'border' && hasError,
           ['text-2xl']: props.size === 'lg',
           ['text-[15px]']: props.size === 'md',
         }}
